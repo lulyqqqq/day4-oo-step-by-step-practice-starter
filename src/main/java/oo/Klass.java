88528eaa6b45db5a2bca3f7500e2ed72658d3592
Mason ma <MAMA14@oocl.com>
2024-11-21 21:51:58 +0800

    feat:add attach teacher and attach student,notifyMembers function

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,13 +1,19 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Klass {
     private int id;
     private Student leader;
+    private List<Student> studentList = null;
+    private List<Teacher> teacherList = null;
 
     public Klass(int id) {
         this.id = id;
+        studentList = new ArrayList<>();
+        teacherList = new ArrayList<>();
     }
 
     public int getId() {
@@ -23,11 +29,48 @@ public class Klass {
             System.out.println("It is not one of us.");
         } else {
             this.leader = student;
+            notifyMembers();
         }
     }
 
     public boolean isLeader(Student student) {
         return Objects.equals(this.leader, student);
+    }
+
+    public void attach(Student student) {
+        if (!studentList.contains(student)) {
+            studentList.add(student);
+        }
+    }
+
+    public void attach(Teacher teacher) {
+        if (!teacherList.contains(teacher)) {
+            teacherList.add(teacher);
+        }
+    }
+
+    private void notifyMembers() {
+        if (!teacherList.isEmpty()) {
+            for (Teacher teacher : teacherList) {
+                System.out.printf(
+                        "I am %s, teacher of Class %d. I know %s become Leader.",
+                        teacher.getName(), this.id, leader.getName()
+                );
+            }
+        }
+
+        if (!studentList.isEmpty()) {
+            for (Student student : studentList) {
+                if (student != leader) {
+                    System.out.printf(
+                            "I am %s, student of Class %d. I know %s become Leader.",
+                            student.getName(), this.id, leader.getName()
+                    );
+                }
+            }
+
+        }
+
     }
 
     @Override
