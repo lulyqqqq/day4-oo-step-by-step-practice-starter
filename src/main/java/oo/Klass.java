d60f4a582a928d0ab5257d88afe770bff92cbf84
Mason ma <MAMA14@oocl.com>
2024-11-21 14:13:08 +0800

    feat:add step 3 logic and test Klass

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,5 +1,26 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+    private int id;
+
+    public Klass(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object klass) {
+        if (klass == null) {
+            return false;
+        }
+        Klass nowKlass = (Klass) klass;
+        return Objects.equals(this.id, nowKlass.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id);
+    }
 
 }
