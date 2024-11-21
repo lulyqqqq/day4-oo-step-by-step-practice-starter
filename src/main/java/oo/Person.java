eb962608e9239bb95f65ba3751422c259d0c78b9
Mason ma <MAMA14@oocl.com>
2024-11-21 13:54:43 +0800

    feat: overwire equals and hashCode

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,5 +1,7 @@
 package oo;
 
+import java.util.Objects;
+
 public class Person {
     private int id;
     private String name;
@@ -13,5 +15,19 @@ public class Person {
 
     public String introduce() {
         return String.format("My name is %s. I am %d years old.", name, age);
+    }
+
+    @Override
+    public boolean equals(Object person) {
+        if (person == null) {
+            return false;
+        }
+        Person nowPerson = (Person) person;
+        return Objects.equals(this.id, nowPerson.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id);
     }
 }
